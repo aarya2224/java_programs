1a866471a1da6c31ad84dc62fff9168a97f26543
aarya2224 <jadhavaarya9421@gmail.com>
2026-05-06 13:46:21 +0530

    Added Test class with main method for Calculator

---
+++ return_statement/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+	public static void main(String[] args) {
+		Calculator c1 = new Calculator();
+		int s = c1.addition(25, 100, 75);
+		System.out.println("Addition= "+s);
+		int a = c1.average(50, 70, 90);
+		System.out.println("Average= "+a);
+	
+		}
+}
